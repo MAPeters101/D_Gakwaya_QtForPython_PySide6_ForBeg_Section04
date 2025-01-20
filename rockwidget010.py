1bfd622384f10ba6811a3b22d11de435c0c37174
Mark Peters <mapeters101@earthlink.net>
2025-01-19 23:46:45 -0500

    Work on lesson010 to 5:20.

--- rockwidget010.py
+++ rockwidget010.py
@@ -1,6 +1,7 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QPushButton
 
 class RockWidget(QWidget):
   def __init__(self):
     super().__init__()
     self.setWindowTitle("RockWidget")
+    button1 = QPushButton("Button1")
