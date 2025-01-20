830142cc5dd575e0bc19b307b827f7d2b859f308
Mark Peters <mapeters101@earthlink.net>
2025-01-20 15:04:35 -0500

    Work on lesson010 to 8:00: horizontal layout.

--- rockwidget010.py
+++ rockwidget010.py
@@ -1,7 +1,18 @@
-from PySide6.QtWidgets import QWidget, QPushButton
+from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout
 
 class RockWidget(QWidget):
   def __init__(self):
     super().__init__()
     self.setWindowTitle("RockWidget")
     button1 = QPushButton("Button1")
+    button2 = QPushButton("Button2")
+
+    button_layout = QHBoxLayout()
+    button_layout.addWidget(button1)
+    button_layout.addWidget(button2)
+
+    self.setLayout(button_layout)
+
+
+
+
