74ac0f59f59cce4f3127726517a31c234f37e07b
Mark Peters <mapeters101@earthlink.net>
2025-01-20 19:10:01 -0500

    Work on lesson012 to 9:00: Setup main window with buttons.

--- widget012.py
+++ widget012.py
@@ -3,3 +3,47 @@ from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QMessageBox
 class Widget(QWidget):
   def __init__(self):
     super().__init__()
+
+    self.setWindowTitle("QMessageBox")
+
+    button_hard = QPushButton("Hard")
+    button_hard.clicked.connect(self.button_clicked_hard)
+
+    button_critical = QPushButton("Critical")
+    button_critical.clicked.connect(self.button_clicked_critical)
+
+    button_question = QPushButton("Question")
+    button_question.clicked.connect(self.button_clicked_question)
+
+    button_information = QPushButton("Information")
+    button_information.clicked.connect(self.button_clicked_information)
+
+    button_warning = QPushButton("Warning")
+    button_warning.clicked.connect(self.button_clicked_warning)
+
+    button_about = QPushButton("About")
+    button_about.clicked.connect(self.button_clicked_about)
+
+    # Set up the layouts
+    layout = QVBoxLayout()
+    layout.addWidget(button_hard)
+    layout.addWidget(button_critical)
+    layout.addWidget(button_question)
+    layout.addWidget(button_information)
+    layout.addWidget(button_warning)
+    layout.addWidget(button_about)
+    self.setLayout(layout)
+
+  def button_clicked_hard(self):
+    print("Hard")
+  def button_clicked_critical(self):
+    print("Critical")
+  def button_clicked_question(self):
+    print("Question")
+  def button_clicked_information(self):
+    print("Information")
+  def button_clicked_warning(self):
+    print("Warning")
+  def button_clicked_about(self):
+    print("About")
+
