ccee846b02bd9c337b024ae8fda76e7918156499
Mark Peters <mapeters101@earthlink.net>
2025-01-21 18:24:56 -0500

    Work on lesson014 to 9:30:  Add base controls.

---
+++ widget014.py
@@ -0,0 +1,25 @@
+from PySide6.QtWidgets import QWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QLabel, QPushButton
+
+class Widget(QWidget):
+  def __init__(self):
+    super().__init__()
+
+    self.setWindowTitle("QLabel and QLineEdit")
+
+    # A set of signals we can connect to
+    label = QLabel("Fullname: ")
+    self.line_edit = QLineEdit(label)
+
+    button = QPushButton("Grab data")
+    self.text_holder_label = QLabel("I AM HERE")
+
+    h_layout = QHBoxLayout()
+    h_layout.addWidget(label)
+    h_layout.addWidget(self.line_edit)
+
+    v_layout = QVBoxLayout()
+    v_layout.addLayout(h_layout)
+    v_layout.addWidget(button)
+    v_layout.addWidget(self.text_holder_label)
+
+    self.setLayout(v_layout)
