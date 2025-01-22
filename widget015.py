72ad66ed44baa5e1827f2dde759dd5aabdead5fe
Mark Peters <mapeters101@earthlink.net>
2025-01-22 13:53:20 -0500

    Work on lesson015 to 12:50:  Past via the text_edit widget.

--- widget015.py
+++ widget015.py
@@ -20,7 +20,8 @@ class Widget(QWidget):
     cut_button.clicked.connect(self.text_edit.cut)
 
     paste_button = QPushButton("Paste")
-    #paste_button.clicked.connect(self.paste) # Go through a custom slot
+    paste_button.clicked.connect(self.text_edit.paste)
+    #paste_button.clicked.connect(self.text_edit.paste) # Go through a custom slot
 
     undo_button = QPushButton("Undo")
     undo_button.clicked.connect(self.text_edit.undo)
