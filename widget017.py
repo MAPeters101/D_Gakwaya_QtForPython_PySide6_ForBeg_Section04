a6c00ffcaa36b54bb37f0e9b69578c5ac4ba3fdf
Mark Peters <mapeters101@earthlink.net>
2025-01-23 16:26:13 -0500

    Work on lesson017 to 8:30:  Add widgets to app.

--- widget017.py
+++ widget017.py
@@ -1,7 +1,35 @@
-from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QSizePolicy
+from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QSizePolicy, QLineEdit, QPushButton
 
 class Widget(QWidget):
   def __init__(self):
     super().__init__()
 
     self.setWindowTitle("Size policies and stretches")
+
+    #Size policy: how the widgets behaves if container space is expanded or shrunk
+    label = QLabel("Some text: ")
+    line_edit = QLineEdit()
+
+    h_layout_1 = QHBoxLayout()
+    h_layout_1.addWidget(label)
+    h_layout_1.addWidget(line_edit)
+
+    button_1 = QPushButton("One")
+    button_2 = QPushButton("Two")
+    button_3 = QPushButton("Three")
+
+    # stretch: how much the available space (in the layout) is occupied by each widget
+    # You specify the stretch when you add things to the layout: Button_1 takes up 2 units,
+    # button_2 and button_3 each take up 1 unit.
+
+    h_layout_2 = QHBoxLayout()
+    h_layout_2.addWidget(button_1)
+    h_layout_2.addWidget(button_2)
+    h_layout_2.addWidget(button_3)
+
+    v_layout = QVBoxLayout()
+    v_layout.addLayout(h_layout_1)
+    v_layout.addLayout(h_layout_2)
+
+    self.setLayout(v_layout)
+
