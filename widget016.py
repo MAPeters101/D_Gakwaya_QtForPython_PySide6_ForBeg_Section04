49194c04feb0d241ca0d5b5d401471cdedaec00a
Mark Peters <mapeters101@earthlink.net>
2025-01-22 19:14:04 -0500

    Complete lesson016: Add image.

--- widget016.py
+++ widget016.py
@@ -5,5 +5,16 @@ class Widget(QWidget):
   def __init__(self):
     super().__init__()
 
+    self.setWindowTitle("QLabel Image Demo")
+
+    image_label = QLabel()
+    image_label.setPixmap(QPixmap("images/minions.png"))
+
+    layout = QVBoxLayout()
+    layout.addWidget(image_label)
+
+    self.setLayout(layout)
+
+
 
   
