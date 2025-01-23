80ce3fa8b6d719696295c5e4fd721d1d5fd2ea95
Mark Peters <mapeters101@earthlink.net>
2025-01-22 19:06:21 -0500

    Work on lesson016 to 2:00:  Add base application.

---
+++ widget016.py
@@ -0,0 +1,9 @@
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QPushButton
+
+class Widget(QWidget):
+  def __init__(self):
+    super().__init__()
+
+
+  
