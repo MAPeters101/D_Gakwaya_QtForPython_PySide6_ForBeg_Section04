733e2cd56be454fe5589965401f02a6eb5a8a278
Mark Peters <mapeters101@earthlink.net>
2025-01-24 16:06:19 -0500

    Work on lesson019 to 6:00: add checkboxes/

--- widget019.py
+++ widget019.py
@@ -6,3 +6,20 @@ class Widget(QWidget):
     super().__init__()
 
     self.setWindowTitle("QCheckBox and QRadioButton")
+
+    # Checkboxes: operating system
+    os = QGroupBox("Choose operating system")
+    windows = QCheckBox("Windows")
+    windows.toggled.connect(self.windows_box_toggled)
+
+    linux = QCheckBox("Linux")
+    linux.toggled.connect(self.linux_box_toggled)
+
+    mac = QCheckBox("Mac")
+    mac.toggled.connect(self.max_box_toggled)
+    os_layout = QVBoxLayout()
+    os_layout.addWidget(windows)
+    os_layout.addWidget(linux)
+    os_layout.addWidget(mac)
+    os.setLayout(os_layout)
+
