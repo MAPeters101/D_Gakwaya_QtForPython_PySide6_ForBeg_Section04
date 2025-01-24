ac1a9dbb7b41e7fad1e11cd81134d36155bbf716
Mark Peters <mapeters101@earthlink.net>
2025-01-24 16:23:24 -0500

    Work on lesson019 to 7:20: Add layout.

--- widget019.py
+++ widget019.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QCheckBox, QHBoxLayout, QVBoxLayout, QSizePolicy, QGridLayout, QPushButton
+from PySide6.QtWidgets import QWidget, QCheckBox, QHBoxLayout, QVBoxLayout, QSizePolicy, QGridLayout, QPushButton, QGroupBox
 
 
 class Widget(QWidget):
@@ -10,16 +10,21 @@ class Widget(QWidget):
     # Checkboxes: operating system
     os = QGroupBox("Choose operating system")
     windows = QCheckBox("Windows")
-    windows.toggled.connect(self.windows_box_toggled)
+    #windows.toggled.connect(self.windows_box_toggled)
 
     linux = QCheckBox("Linux")
-    linux.toggled.connect(self.linux_box_toggled)
+    #linux.toggled.connect(self.linux_box_toggled)
 
     mac = QCheckBox("Mac")
-    mac.toggled.connect(self.max_box_toggled)
+    #mac.toggled.connect(self.max_box_toggled)
     os_layout = QVBoxLayout()
     os_layout.addWidget(windows)
     os_layout.addWidget(linux)
     os_layout.addWidget(mac)
     os.setLayout(os_layout)
+
+    layout = QVBoxLayout()
+    layout.addWidget(os)
+
+    self.setLayout(layout)
 
